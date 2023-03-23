f572e55d5e7ee73b494ccc38f6178c66498a100e
zeidk <zeidk@umd.edu>
2023-03-22 22:03:11 -0400

    updated readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AV Simulation Testbed'
+copyright = '2023, NIST'
+author = 'Z. Kootbally'
 
 release = '0.1'
 version = '0.1.0'
